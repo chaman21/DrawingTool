8af3361537e48d12155a3b720c7feb35a3d251a3
Chaman <chaman.jain888@gmail.com>
2017-07-30 19:25:57 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// 1. Express requires these dependencies
+var express = require('express')
+  , routes = require('./routes')
+//  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// 2. Configure our application
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+// 3. Configure error handling
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// 4. Setup Routes
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+// 5. Enable Socket.io
+var server = http.createServer(app).listen( app.get('port') );
+var io = require('socket.io').listen(server, function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+// A user connects to the server (opens a socket)
+io.sockets.on('connection', function (socket) {
+
+    // (2): The server recieves a ping event
+    // from the browser on this socket
+    socket.on('ping', function ( data ) {
+
+    console.log('socket: server recieves ping (2)');
+
+    // (3): Return a pong event to the browser
+    // echoing back the data from the ping event
+    socket.emit( 'pong', data );
+
+    console.log('socket: server sends pong (3)');
+
+    });
+});
